e22162722c6570aa17cf130a87cd5e3414d73320
Sanz Díaz Alejandro (ERNI) <saae@erni-espana.es>
2021-04-12 19:41:47 +0200

    add filtered books list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,6 +5,14 @@
       <book-item v-for="book in books" :book="book" v-bind:key="book.title"></book-item>
       <book-form @addBook="appendBook"></book-form>
     </ul>
+    <hr>
+    <h2>Filtered Books By Ownership</h2>
+    <select name="holding" id="holding" v-model="holding">
+      <option v-for="filter in filters" v-bind:key="filter" :value="filter">{{ filter }}</option>
+    </select>
+        <ul>
+      <book-item v-for="book in filteredBooks" :book="book" v-bind:key="book.title"></book-item>
+    </ul>
   </div>
 </template>
 
@@ -12,22 +20,30 @@
 <script>
 import BookItem from './BookItem';
 import BookForm from './BookForm';
+import _ from "lodash";
 
 export default {
   name: "BookList",
-  data: () => {
+  data() {
     return {
       title: "All Books",
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
-      ]
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: "borrowed" },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: "borrowed" }
+      ],
+      filters: ["bought", "borrowed"],
+      holding: "bought"
     };
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ['ownership', this.holding]);
+    }
   },
   methods: {
       appendBook: function (bookData) {
-          this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading});
+          this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading, ownership: bookData.ownership});
       }
   },
   components: {
